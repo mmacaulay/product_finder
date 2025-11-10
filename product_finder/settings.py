475e9acc58b45275984a8e34fe1d17b0e3576561
Matt MacAulay <matt@m10y.org>
2025-11-10 16:14:53 -0500

    Postgres support

--- product_finder/settings.py
+++ product_finder/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'product_finder.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "product_finder_dev",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "localhost",
+        "PORT": "6000",
     }
 }
 
